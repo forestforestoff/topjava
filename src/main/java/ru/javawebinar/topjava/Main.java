cddf81061afb7212a121d09d0269f2b4ed45b599
forestforestoff <forestforestoff@yandex.ru>
2019-03-06 21:13:40 +0300

    *HW0* ~realised~ UserMealsUtil.getFilteredWithExceeded UserMealsUtil.getFilteredWithExceededByStream ~improvements~ UserMealWithExceed.toString

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,11 +1,31 @@
 package ru.javawebinar.topjava;
 
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.UserMealsUtil.getFilteredWithExceeded;
+import static ru.javawebinar.topjava.util.UserMealsUtil.getFilteredWithExceededByStream;
+
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+        System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(getFilteredWithExceededByStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 }
